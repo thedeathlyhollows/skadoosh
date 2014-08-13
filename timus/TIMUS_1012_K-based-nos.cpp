bb55d31f2f7ed56cbf6a30f4714bae9a756e32ce
thedeathlyhollows <swagath1308@gmail.com>
2014-08-13 22:39:07 +0530

    timus solns

---
+++ timus/TIMUS_1012_K-based-nos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,k;
+	cin>>n>>k;
+	unsigned long long int a[200][2];
+	a[0][0]=a[0][1]=0;
+	a[1][0]=k-1;a[1][1]=0;
+	for(int i=2;i<=n;i++)
+	{
+		a[i][0]=(a[i-1][0]+a[i-1][1])*(k-1);
+		a[i][1]=a[i-1][0];
+	}
+	cout<<(a[n][0]+a[n][1]);
+	return 0;
+}
